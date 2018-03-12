53bd2ae07b41624ace4702d51e9d97aa495f0b49
NickDErrico <nickderrico@hotmail.com>
2018-03-12 15:43:33 -0700

    grid set-up for tree display

--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -24,7 +24,34 @@ class NewProject extends Component {
                   <DropdownItem>TEST 2</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-
+            </Card>
+            <Card id='initial-component-two'>
+              <p>App</p>
+              <p>Stateful: true</p>
+              <p>Parent: none</p>
+              <UncontrolledDropdown>
+                <DropdownToggle caret>
+                  Children :
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>TEST 1</DropdownItem>
+                  <DropdownItem>TEST 2</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Card>
+            <Card id='initial-component-three'>
+              <p>App</p>
+              <p>Stateful: true</p>
+              <p>Parent: none</p>
+              <UncontrolledDropdown>
+                <DropdownToggle caret>
+                  Children :
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>TEST 1</DropdownItem>
+                  <DropdownItem>TEST 2</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </Card>
           </Col>
           <Col md={3} id='right-display'>
